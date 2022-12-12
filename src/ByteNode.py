845f1fb924fb90a87ecb34e4b1c591aae63c5310
Ofek Shmuel <ofek.shmuel1@gmail.com>
2022-12-12 22:50:29 +0200

    Implemented ByteNode Class

---
+++ src/ByteNode.py
@@ -0,0 +1,20 @@
+from typing import Union, Type
+
+class ByteNode:
+
+    def __init__(self, byte: str):
+        """ Constructor function """
+        if not isinstance(byte, str):
+            raise TypeError("input argument 'byte' is not op type string!")
+        if len(byte) != 8:
+            raise ValueError("input strin 'byte' is not of length 8!")
+        self.byte = byte
+        self.next = None
+
+    def get_byte(self) -> str:
+        """ get the byte field """
+        return self.byte
+
+    def get_next(self) -> Union[None, ByteNode]:
+        """ get the next field """
+        return self.next
