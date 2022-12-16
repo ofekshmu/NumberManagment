6f762c80965af32d9127e0c8b29065aaf92fa035
Ofek Shmuel <ofek.shmuel1@gmail.com>
2022-12-16 16:20:46 +0200

    testing for implemented functions

--- src/test_.py
+++ src/test_.py
@@ -27,6 +27,34 @@ def tests_init_linked_list_binary_num():
         bn1 = LinkedListBinaryNum(i)
         assert bn1.__repr__() == f"LinkedListBinaryNum with 2 Bytes, Bytes map: [{bin(i)[2:].zfill(16)[:8]}]=>[{bin(i)[2:].zfill(16)[8:]}]=>None"
 
+def test_add_msb():
+    bn1 = LinkedListBinaryNum(0)
+    assert bn1.__repr__() == "LinkedListBinaryNum with 1 Byte, Bytes map: [00000000]=>None"
+
+    import random
+    st = ""
+    for i in range(30):
+        byte =  ''.join(random.choices(['0', '1'], k=8))
+        bn1.add_MSB(byte)
+        st = f"[{byte}]=>" + st
+        assert bn1.__repr__() == f"LinkedListBinaryNum with 1 Byte, Bytes map: {st}[00000000]=>None"
 
 
-tests_init_linked_list_binary_num()
+def test_str_func():
+    bn = LinkedListBinaryNum(0)
+    res_str = "00000000|"
+    import random
+    for i in range(50):
+        byte = ''.join(random.choices(['0', '1'], k=8))
+        bn.add_MSB(byte)
+        res_str = f"{byte}|" + res_str
+        assert bn.__str__() == f"|{res_str}"
+
+def test_get_item():
+    bn = LinkedListBinaryNum(12345678901234567890)
+    st = bn.__str__()
+    lst = st.split("|")[1:-1]
+    for i in range(len(lst) - 1):
+        assert bn[i] == lst[i]
+        assert bn[i] == bn[- bn.size + i]
+        
